c72d1f7f58b10b45ba15e3c740e8e0f86f822729
goatfishplays <goatfishplays@gmail.com>
2024-04-27 17:41:47 -0700

    node list stuff

--- NodeList.py
+++ NodeList.py
@@ -1,16 +1,60 @@
 import Nodes
 
+
 class NodeHolder:
-    def __init__(self, node, userPos):
+    def __init__(self, node: Nodes.Node, userPos: int):
         self.node = node
         self.userPos = userPos
 
+    def __str__(self):
+        return f"{self.userPos}\t{self.node}"
+
+
 class List:
-    def __init__(self, nodes):
-        self.nodes: list = [NodeHolder(b, a) for a,b in enumerate(nodes)]
+    def __init__(self, name, nodes: list[Nodes.Node]):
+        self.name = name
+        self.nodes: list[NodeHolder] = [NodeHolder(b, a) for a, b in enumerate(nodes)]
+        for node in nodes:
+            node.lists.add(name)
+
+    def sort(self, sortType: str = "", reverse: bool = False):
+        if sortType == "time":
+            self.nodes.sort(key=lambda x: x.node.getTimeSecs())
+        else:
+            self.nodes.sort(key=lambda x: x.userPos)
+        if reverse:
+            self.nodes.reverse()
+
+    def removeNode(self, node: Nodes.Node):
+        for i in range(len(self.nodes)):
+            if self.nodes[i].node == node:
+                node.lists.remove(self.name)
+                self.nodes.pop(i)
+                return
+
+    def __str__(self):
+        sAcc = f"{self.name}\nOrder\tName\tNotes\tLists\tShared\tCompletable\tCompletion\tTime\n"
+        for node in self.nodes:
+            sAcc += str(node) + "\n"
+        sAcc.rstrip()
+        return sAcc
 
 
-    def sort(self, sortType):
-        match sortType:
-            case "time":
-                self.nodes.sort(key )
+if __name__ == "__main__":
+    a = Nodes.Node("thing", time=Nodes.Time(8, 8, 8))
+    thing = List(
+        "A List",
+        [
+            Nodes.Node("hats", lists={"klfja", "kajsdf;lka"}, time=Nodes.Time(15, 20, 70)),
+            Nodes.Node("beans", time=Nodes.Time(10, 5, 2)),
+            Nodes.Node("Cheese", time=Nodes.Time(11, 51, 21)),
+            a,
+            Nodes.Node("mmm"),
+        ],
+    )
+    thing2 = List("B List", [a, Nodes.Node("chee")])
+    print(thing)
+    thing.sort("time")
+    print(thing)
+    print()
+    print(thing2)
--- Nodes.py
+++ Nodes.py
@@ -9,27 +9,32 @@ class Time:
         self.reformat()
 
     def reformat(self):
-        self.mins += self.secs // 60
-        self.secs %= 60
-        self.hours += self.mins // 60
-        self.mins %= 60
+        if self.secs >= 60:
+            self.mins += self.secs // 60
+            self.secs %= 60
+        if self.mins >= 60:
+            self.hours += self.mins // 60
+            self.mins %= 60
 
     def __str__(self):
         return f"{self.hours:02}:{self.mins:02}:{self.secs:02}"
 
 
 class Node:
-    def __init__(self, name, notes, lists, shared, completable=False, completion=0, time=Time()):
+    def __init__(self, name="No Name", notes="No Notes", lists=set(), shared=False, completable=False, completion=0, time=Time()):
         self.name = name
         self.notes = notes
-        self.lists = lists
+        self.lists = lists if len(lists) > 0 else set()
         self.shared = shared
         self.completable = completable
         self.completion = completion
         self.time = time
 
+    def __str__(self):
+        return f"{self.name}\t{self.notes}\t{self.lists}\t{self.shared}\t{self.completable}\t{self.completion}\t{self.time}"
+
     def getRelativeTime(self):
-        t = time.gmtime()
+        t = time.localtime()
         hours = self.time.hours - t.tm_hour
         mins = self.time.mins - t.tm_min
         secs = self.time.secs - t.tm_sec
@@ -42,11 +47,14 @@ class Node:
         return (hours, mins, secs)
 
     def setRelativeTime(self, hours, mins, sec):
-        t = time.gmtime()
+        t = time.localtime()
         self.time.hours = t.tm_hour + hours
         self.time.mins = t.tm_min + mins
         self.time.secs = t.tm_sec + sec
         self.time.reformat()
+
+    def getTimeSecs(self):
+        return self.time.secs + self.time.mins * 60 + self.time.hours * 3600
 
 
 # class TimeNode(Node):
@@ -59,8 +67,10 @@ class Node:
 # }
 
 if __name__ == "__main__":
-    a = Node("A Node", "This node is a node", [], shared=False, completable=False, completion=0, time=Time(16, 22, 30))
+    t = time.localtime()
+    a = Node("A Node", "This node is a node", set(), shared=False, completable=False, completion=0, time=Time(t.tm_hour, t.tm_min, t.tm_sec))
     print(a.getRelativeTime())
     print(a.time)
     a.setRelativeTime(5, 5, 5)
+    print(a.getRelativeTime())
     print(a.time)
